f27ae4b7407f4aae52edfda6225bc613be73c158
anatonokao <anato.devs@gmail.com>
2023-11-14 22:10:05 +0300

    feat: add basic layout, add styles, create page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import AuthPage from './pages/AuthPage/AuthPage';
+import HomePage from './pages/HomePage/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="/" element={<HomePage />} />
+      <Route path="/auth" element={<AuthPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <RouterProvider router={router} />
     </div>
   );
 };
---
+++ src/pages/AuthPage/AuthPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthPage = () => {
+  return <div>Auth page work</div>;
+};
+
+export default AuthPage;
